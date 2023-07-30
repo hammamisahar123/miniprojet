31c48b4fbadc032e2ea64d6021d6cc34433353f9
hammamisahar123 <hammamisahar456@gmail.com>
2023-07-30 23:36:03 +0200

    message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 import Navbar from './components/Navbar';
 import Home from './page/Home';
+import Login from './page/Login';
 
 
 function App() {
   return (
     <div className="bg-gray-400 min-h-screen">
+      <Login />
       <Navbar />
       <Home />
     </div>
+    
+
   );
 }
 
